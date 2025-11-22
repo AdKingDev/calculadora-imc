5ac62dab74f8b46e4fe4f746637e5bdbd1da8f6d
Adriano Júnio <a.rodrigues.sobrinho4@gmail.com>
2025-11-21 22:15:07 -0300

    Primeiro commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+const calcular = document.getElementById('calcular');
+
+function imc() {
+    const nome = document.getElementById('nome');
+    const altura = document.getElementById('altura');
+    const peso = document.getElementById('peso');
+    const resultado = document.getElementById('resultado');
+
+    if (nome.value !== '') {
+
+    }
+
+
+// Função para calcular o IMC
+calcular.addEventListener('click', imc);
